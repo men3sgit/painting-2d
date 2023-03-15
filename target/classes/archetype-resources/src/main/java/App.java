1b23dbc0227e7cf0e8c011b2bd61cb608a03cc16
Duong Duy Men <20130072@st.hcmuaf.edu.vn>
2023-03-15 20:19:54 +0700

    add icons

---
+++ target/classes/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $com.menes.projects;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
